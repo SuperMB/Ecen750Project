d6787d6302c5def6520f94ad6c4557d5cd54bd2c
SuperMB <littlebass09@tamu.edu>
2017-12-12 15:18:04 -0600

    MB Moving to Desktop

---
+++ InterleaveSimulation/InterleaveSimulation/Network.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterleaveSimulation
+{
+    class Network
+    {
+        public Network()
+        {
+            TimeSlot = 0;
+            _nextServerSection = 0;
+            _finished = false;
+        }
+
+        public void MakeUsers()
+        {
+            Users = new User[NumberOfUsers];
+            for (int i = 0; i < Users.Length; i++)
+            {
+                Users[i] = new User(i);
+                User user = Users[i];
+                user.File = new File(NumberOfSections);
+            }
+        }
+
+        public void CreateContactLists()
+        {
+            foreach (User user in Users)
+            {
+                user.ContactList = new User[ContactListSize];
+                for (int i = 0; i < ContactListSize; i++)
+                {
+                    bool contactAdded = false;
+                    while (!contactAdded)
+                    {
+                        User contact = Users[Functions.Random.Next(Users.Length)];
+                        bool contactAlreadyAdded = false;
+                        if (contact != user)
+                        {
+                            foreach (User addedContact in user.ContactList)
+                                if (addedContact == contact)
+                                    contactAlreadyAdded = true;
+
+                            if (!contactAlreadyAdded)
+                            {
+                                contactAdded = true;
+                                for (int j = 0; j < ContactListSize; j++)
+                                    if (user.ContactList[j] == null)
+                                    {
+                                        user.ContactList[j] = contact;
+                                        break;
+                                    }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public void CreateServer()
+        {
+            Server = new User(-1);
+            Server.File = new File(NumberOfSections);
+            for (int i = 0; i < NumberOfSections; i++)
+                Server.File.InsertSection(new FileSection(i));
+        }
+
+        public void DistributeFile()
+        {
+            while (!_finished)
+            {
+                OddSection();
+                EvenSection();
+            }
+        }
+
+        public void OddSection()
+        {
+            if (CheckForCompletion())
+                return;
+
+            foreach (User user in Users)
+                user.MoveToOddSection();
+
+            TimeSlot++;
+
+            //server pushing out the next section
+            if (_nextServerSection < NumberOfSections)
+            {
+                Users[Functions.Random.Next(Users.Length)].ReceivePush(Server.File.FileSections[_nextServerSection]);
+                _nextServerSection++;
+            }
+
+            foreach (User user in Users)
+                user.Push();
+        }
+
+        public void EvenSection()
+        {
+            if (CheckForCompletion())
+                return;
+
+            foreach (User user in Users)
+                user.MoveToEvenSection();
+
+            TimeSlot++;
+
+            foreach (User user in Users)
+                user.Pull();
+        }
+
+        public bool CheckForCompletion()
+        {
+            foreach (User user in Users)
+                if (!user.File.Completed)
+                    return false;
+
+            _finished = true;
+            return true;
+        }
+
+        public const int NumberOfUsers = 500;
+        public const int NumberOfSections = 1000;
+        public const int ContactListSize = 3;
+        public User[] Users { get; set; }
+        public User Server { get; set; }
+        public int TimeSlot { get; private set; }
+        private bool _finished;
+        private int _nextServerSection;
+    }
+}
--- InterleaveSimulation/InterleaveSimulation/Program.cs
+++ InterleaveSimulation/InterleaveSimulation/Program.cs
@@ -8,137 +8,20 @@ namespace InterleaveSimulation
 {
     class Program
     {
-        private static int _numberOfUsers = 500;
-        private static int _numberOfSections = 1000;
-        private static int _contactListSize = 3;
-        private static User[] _users;
-        private static User _server;
-        private static int _timeSlot;
-        private static bool _finished;
-        private static int _nextServerSection;
-
         static void Main(string[] args)
         {
-            _timeSlot = 0;
-            _nextServerSection = 0;
-            _finished = false;
+            Network network = new Network();
 
-            MakeUsers();
-            CreateContactLists();
-            CreateServer();
+            network.MakeUsers();
+            network.CreateContactLists();
+            network.CreateServer();
 
-            DistributeFile();
+            network.DistributeFile();
+
             Console.Out.WriteLine("Finished");
-            Console.Out.WriteLine($"Final Time Slot: {_timeSlot}.");
+            Console.Out.WriteLine($"Final Time Slot: {network.TimeSlot}.");
 
             Console.Read();
-        }
-
-        private static void MakeUsers()
-        {
-            _users = new User[_numberOfUsers];
-            for (int i = 0; i < _users.Length; i++)
-            {
-                _users[i] = new User(i);
-                User user = _users[i];
-                user.File = new File(_numberOfSections);
-            }
-        }
-
-        private static void CreateContactLists()
-        {
-            foreach (User user in _users)
-            {
-                user.ContactList = new User[_contactListSize];
-                for (int i = 0; i < _contactListSize; i++)
-                {
-                    bool contactAdded = false;
-                    while (!contactAdded)
-                    {
-                        User contact = _users[Functions.Random.Next(_users.Length)];
-                        bool contactAlreadyAdded = false;
-                        if (contact != user)
-                        {
-                            foreach (User addedContact in user.ContactList)
-                                if (addedContact == contact)
-                                    contactAlreadyAdded = true;
-
-                            if (!contactAlreadyAdded)
-                            {
-                                contactAdded = true;
-                                for(int j = 0; j < _contactListSize; j++)
-                                    if(user.ContactList[j] == null)
-                                    {
-                                        user.ContactList[j] = contact;
-                                        break;
-                                    }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        private static void CreateServer()
-        {
-            _server = new User(-1);
-            _server.File = new File(_numberOfSections);
-            for (int i = 0; i < _numberOfSections; i++)
-                _server.File.InsertSection(new FileSection(i));
-        }
-
-        private static void DistributeFile()
-        {
-            while(!_finished)
-            {
-                OddSection();
-                EvenSection();
-            }
-        }
-
-        private static void OddSection()
-        {
-            if (CheckForCompletion())
-                return;
-
-            foreach (User user in _users)
-                user.MoveToOddSection();
-
-            _timeSlot++;
-
-            //server pushing out the next section
-            if (_nextServerSection < _numberOfSections)
-            {
-                _users[Functions.Random.Next(_users.Length)].ReceivePush(_server.File.FileSections[_nextServerSection]);
-                _nextServerSection++;
-            }
-
-            foreach (User user in _users)
-                user.Push();                
-        }
-
-        private static void EvenSection()
-        {
-            if (CheckForCompletion())
-                return;
-
-            foreach (User user in _users)
-                user.MoveToEvenSection();
-
-            _timeSlot++;
-
-            foreach (User user in _users)
-                user.Pull();
-        }
-
-        private static bool CheckForCompletion()
-        {
-            foreach (User user in _users)
-                if (!user.File.Completed)
-                    return false;
-
-            _finished = true;
-            return true;
         }
     }
 }
